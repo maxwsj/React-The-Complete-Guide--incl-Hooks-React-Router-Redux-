875b8851b3ee999812a0524bcf761888c065cf91
Max William <83377057+maxwsj@users.noreply.github.com>
2022-05-25 13:50:00 -0300

    Advanced Redux Lessons

--- Advanced Redux/starting-project/src/App.js
+++ Advanced Redux/starting-project/src/App.js
@@ -4,7 +4,7 @@ import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 import Notification from './components/UI/Notification';
-import { uiActions } from './store/ui-slice';
+import { fetchCartData, sendCartData } from './store/cart-actions';
 
 let isInitial = true;
 
@@ -17,46 +17,18 @@ function App() {
    const dispatch = useDispatch();
 
    useEffect(() => {
-      async function sendCartData() {
-         dispatch(
-            uiActions.showNotification({
-               status: 'pending',
-               title: 'Sending...',
-               message: 'Sending cart data!',
-            })
-         );
-         const response = await fetch(
-            'https://react-lessons-8cbae-default-rtdb.firebaseio.com/cart.json',
-            { method: 'PUT', body: JSON.stringify(cart) }
-         );
+      dispatch(fetchCartData());
+   }, [dispatch]);
 
-         if (!response.ok) {
-            throw new Error('Sending cart data failed.');
-         }
-
-         dispatch(
-            uiActions.showNotification({
-               status: 'success',
-               title: 'Success!',
-               message: 'Sent cart data successfully!!',
-            })
-         );
-      }
-
+   useEffect(() => {
       if (isInitial) {
          isInitial = false;
          return;
       }
 
-      sendCartData().catch((error) => {
-         dispatch(
-            uiActions.showNotification({
-               status: 'error',
-               title: 'Error!',
-               message: 'Sending cart data failed!!',
-            })
-         );
-      });
+      if (cart.changed) {
+         dispatch(sendCartData(cart));
+      }
    }, [cart, dispatch]);
 
    return (
--- Advanced Redux/starting-project/src/store/cart-slice.js
+++ Advanced Redux/starting-project/src/store/cart-slice.js
@@ -3,6 +3,7 @@ import { createSlice } from '@reduxjs/toolkit';
 const cartSlice = createSlice({
    name: 'cart',
    initialState: { items: [], totalQuantity: 0 },
+   changed: false,
    reducers: {
       replaceCart(state, action) {
          state.totalQuantity = action.payload.totalQuantity;
@@ -14,6 +15,7 @@ const cartSlice = createSlice({
             (item) => item.id === newItem.id
          );
          state.totalQuantity++;
+         state.changed = true;
          if (!existingItem) {
             state.items.push({
                id: newItem.id,
@@ -31,6 +33,7 @@ const cartSlice = createSlice({
          const id = action.payload;
          const existingItem = state.items.find((item) => item.id === id);
          state.totalQuantity--;
+         state.changed = true;
          if (existingItem.quantity === 1) {
             state.items = state.items.filter((item) => item.id !== id);
          } else {
